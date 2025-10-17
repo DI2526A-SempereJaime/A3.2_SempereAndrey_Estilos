65a33fbdab5e3d484cf88d6a1bc00dd66bad795e
LordCardinal505 <jaimesempere63@gmail.com>
2025-10-17 13:54:54 +0200

    Ejercicio 2 (Solo falta el texto blanco)

---
+++ A3.2.2_EquiposNBA_Estilos/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A3._2._2_EquiposNBA_Estilos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
