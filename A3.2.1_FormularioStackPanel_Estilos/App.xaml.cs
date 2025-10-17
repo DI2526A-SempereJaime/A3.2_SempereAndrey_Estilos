d21e172c28ba596891e7449a1e82bcc26fd9c621
LordCardinal505 <jaimesempere63@gmail.com>
2025-10-17 12:49:15 +0200

    Ejercicio 1

---
+++ A3.2.1_FormularioStackPanel_Estilos/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A3._2._1_FormularioStackPanel_Estilos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
