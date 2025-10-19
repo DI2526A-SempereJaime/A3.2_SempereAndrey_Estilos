60a8c96c177661754aa242808c89cb49c09b3e1f
LordCardinal505 <jaimesempere63@gmail.com>
2025-10-19 15:13:39 +0200

    Add files via upload

---
+++ A3.2.3_VisorImagenes_Estilos/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A3._1._4_VisorImagenes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ A3.2.4_Calculadora_Estilos/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A3._2._4_Calculadora_Estilos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
